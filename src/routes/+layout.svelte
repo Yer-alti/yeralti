37936eb8516b09da9fc61fa6b1ac9772be4b3dca
Carabelli <carabelli@192.168.1.11>
2024-12-28 21:39:49 +0300

    footer unused connectors removed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -88,17 +88,4 @@
 </div>
 
 <style>
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
 </style>
